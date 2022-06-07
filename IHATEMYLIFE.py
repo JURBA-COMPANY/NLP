ae0203fdb376e7915d6e0b19b1f1d85407527ed1
Vladislav <90088322+Asssmodei@users.noreply.github.com>
2022-06-07 20:06:53 +0300

    Update IHATEMYLIFE.py

--- IHATEMYLIFE.py
+++ IHATEMYLIFE.py
@@ -3,15 +3,15 @@ import torch.nn.functional as f
 import torch.optim as optim
 from gisi import FFUFUDUFUFUF
 from torch.utils.data import DataLoader
-from ignite.engine import Engine, _prepare_batch
+from ignite.engine import Engine, _prepare_batch, Events
 
 
 class Net(nn.Module):
     def __init__(self, love):
         super(Net, self).__init__()
-        self.fc1 = nn.Transformer(love, 200)
-        self.fc2 = nn.Linear(200, 200)
-        self.fc3 = nn.Linear(200, 10)
+        self.fc1 = nn.Embedding(love, 112)
+        self.fc2 = nn.Linear(112, 60)
+        self.fc3 = nn.Linear(60, 10)
 
     def forward(self, x):
         x = f.relu(self.fc1(x))
@@ -21,11 +21,13 @@ class Net(nn.Module):
 
 
 g = FFUFUDUFUFUF()
-love = DataLoader(g, batch_size=7, shuffle=True, num_workers=3, )
+love = DataLoader(g, batch_size=7, shuffle=True, num_workers=1)
+print(type(love))
 net = Net(love)
 lossfunc = nn.CrossEntropyLoss()
 optimizer = optim.Adam(net.parameters(), lr=0.001)
 print('Done!')
+log_interval = 50
 
 
 def process_function(engine, batch, love):
@@ -40,3 +42,4 @@ def process_function(engine, batch, love):
 
 
 trainer = Engine(process_function)
+trainer.run(love, max_epochs=10)
